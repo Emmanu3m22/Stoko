ba968cf21c4d12af15567b6a704358d93cbf346c
Emmanuel Morales Orihuela <l23281383@toluca.tecnm.mx>
2026-04-29 22:44:00 -0600

    Commit con estructura inicial del proyecto Stoko.

---
+++ backend/app/main.py
@@ -0,0 +1,7 @@
+﻿from fastapi import FastAPI
+
+app = FastAPI(title="Stoko API", description="API para el sistema de inventarios y ventas Stoko", version="1.0.0")
+
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenido a Stoko API"}
